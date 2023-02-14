4493f2b547c271c62f71aa37de0de0d1acd732fb
Narae Jang <120549258+NaraeJang@users.noreply.github.com>
2023-02-13 23:05:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 const https = require("https");
-// const request = require("request");
+const { json } = require("body-parser");
+
 
 const app = express();
 
 mailchimp.setConfig({
-    apiKey: "22945d633db56fe7a6d1afca31c764fe-us18",
+    apiKey: "0b9b46d4b5c7445cede47d86848aadfb-us18",
     server: "us18",
 });
 
@@ -32,30 +33,41 @@ run();
 
 app.post("/", function (req, res) {
 
+    //To Add Member in the List
     const subscribingUser = {
         firstName: req.body.fName,
         lastName: req.body.lName,
         email: req.body.email
-    
+
     }
 
 
     const run = async () => {
-        const response = await mailchimp.lists.addListMember("b0cc6ae9b9", {
-            email_address: subscribingUser.email,
-            status: "subscribed",
-            merge_fields: {
-                FNAME: subscribingUser.firstName,
-                LNAME: subscribingUser.lastName
-            }
-        });
-        console.log(response);
+        try {
+            const response = await mailchimp.lists.addListMember("b0cc6ae9b9", {
+                email_address: subscribingUser.email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: subscribingUser.firstName,
+                    LNAME: subscribingUser.lastName
+                }
+            });
+            console.log(response);
+            res.sendFile(__dirname + "/success.html");
+        } catch (err) {
+            console.log("====== ERROR ======");
+            console.log(JSON.parse(err.response.error.text).detail);
+        }
     };
 
     run();
 
 });
 
+
+app.post("/failure", function (req, res) {
+    res.redirect("/");
+});
 
 
 
